c6a7cf98082cfbcbd1f89fa01f9d63c4c8b72d1b
alyssadsouza <alyssa.dsouza63@gmail.com>
2023-12-27 13:32:05 -0500

    clean up data cleaner

--- data_cleaner.py
+++ data_cleaner.py
@@ -5,6 +5,8 @@ from constants import TableHeaders, UnitAmenitiesDict, BuildingAmenitiesDict
 MINIMUM_SQFT_THRESHOLD = 200
 
 ################## Parsing and validation functions #################
+
+
 def parse_bed_value(bed_value):
     if pd.isna(bed_value):
         return None
@@ -15,6 +17,7 @@ def parse_bed_value(bed_value):
         except (ValueError, IndexError):
             return None
     return None
+
 
 def parse_bath_value(bath_value):
     if pd.isna(bath_value) or '0' in bath_value:
@@ -27,16 +30,18 @@ def parse_bath_value(bath_value):
     except (ValueError, IndexError):
         return None
 
+
 def parse_sqft_value(sqft_value):
     if pd.isna(sqft_value) or re.search(r'\d', sqft_value) is None:
         return None
     sqft_value = int(sqft_value.replace(',', '').split(' ')[0])
     return sqft_value if sqft_value >= MINIMUM_SQFT_THRESHOLD else None
 
+
 def parse_price_value(price_value):
     if pd.isna(price_value):
         return None
-    price_value = price_value.replace('$', '').replace(',','')
+    price_value = price_value.replace('$', '').replace(',', '')
     if '—' in price_value:
         price_value = price_value.split('—')[-1]
     try:
@@ -44,15 +49,18 @@ def parse_price_value(price_value):
     except ValueError:
         return None
 
-def parse_amenities(amenities_value, amenities_dict):
-    parsed_amenities = amenities_dict.copy()
+
+def parse_building_amenities(amenities_value):
     if not pd.isna(amenities_value):
-        amenities_value = amenities_value.strip().split(',')
-        for amenity in amenities_value:
-            amenity = amenity.strip()
-            if amenity in parsed_amenities:
-                parsed_amenities[amenity] = 1
-    return parsed_amenities
+        return [amenity.strip() for amenity in amenities_value.split(',') if amenity.strip() in BuildingAmenitiesDict]
+    return None
+
+
+def parse_unit_amenities(amenities_value):
+    if not pd.isna(amenities_value):
+        return [amenity.strip() for amenity in amenities_value.split(',') if amenity.strip() in UnitAmenitiesDict]
+    return None
+
 
 def parse_pets_value(pets_value):
     if pd.isna(pets_value):
@@ -60,50 +68,25 @@ def parse_pets_value(pets_value):
     pets_value = pets_value.lower()
     return 1 if any(pet in pets_value for pet in ['dog', 'cat', 'yes']) else 0
 
+
 # Main function to process the data
 def get_cleaned_data():
     df = pd.read_excel('rental_listings.xlsx')
-    cleaned_list = []
+    df[TableHeaders.BED.value] = df[TableHeaders.BED.value].apply(parse_bed_value)
+    df[TableHeaders.BATH.value] = df[TableHeaders.BATH.value].apply(parse_bath_value)
+    df[TableHeaders.SQFT.value] = df[TableHeaders.SQFT.value].apply(parse_sqft_value)
+    df[TableHeaders.PETS.value] = df[TableHeaders.PETS.value].apply(parse_pets_value)
+    df[TableHeaders.BUILDING_AMENITIES.value] = df[TableHeaders.BUILDING_AMENITIES.value].apply(parse_building_amenities)
+    df[TableHeaders.UNIT_AMENITIES.value] = df[TableHeaders.UNIT_AMENITIES.value].apply(parse_unit_amenities)
 
-    for _, row in df.iterrows():
-        bed_value = parse_bed_value(row[TableHeaders.BED.value])
-        bath_value = parse_bath_value(row[TableHeaders.BATH.value])
-        sqft_value = parse_sqft_value(row[TableHeaders.SQFT.value])
-        price_value = parse_price_value(row[TableHeaders.PRICE.value])
-        unit_amenities = parse_amenities(row[TableHeaders.UNIT_AMENITIES.value], UnitAmenitiesDict)
-        building_amenities = parse_amenities(row[TableHeaders.BUILDING_AMENITIES.value], BuildingAmenitiesDict)
-        pets_value = parse_pets_value(row[TableHeaders.PETS.value])
+    # Flatten out the building amenities into one-hot encoded columns
+    df_exploded = df.explode(TableHeaders.BUILDING_AMENITIES.value)
+    dummies = pd.get_dummies(df_exploded, columns=[TableHeaders.BUILDING_AMENITIES.value], prefix='', prefix_sep='', dtype=int)
+    df = dummies.groupby(dummies.index).max()
 
-        if None in [bed_value, bath_value, sqft_value, price_value]:
-            continue
+    # Flatten out the unit amenities into one-hot encoded columns
+    df_exploded = df.explode(TableHeaders.UNIT_AMENITIES.value)
+    dummies = pd.get_dummies(df_exploded, columns=[TableHeaders.UNIT_AMENITIES.value], prefix='', prefix_sep='', dtype=int)
+    df = dummies.groupby(dummies.index).max()
 
-        cleaned_list.append({
-            TableHeaders.BUILDING.value: row[TableHeaders.BUILDING.value],
-            TableHeaders.ADDRESS.value: row[TableHeaders.ADDRESS.value],
-            TableHeaders.CITY.value: row[TableHeaders.CITY.value],
-            TableHeaders.LISTING.value: row[TableHeaders.LISTING.value],
-            TableHeaders.BED.value: bed_value,
-            TableHeaders.BATH.value: bath_value,
-            TableHeaders.SQFT.value: sqft_value,
-            TableHeaders.PRICE.value: price_value,
-            TableHeaders.UNIT_AMENITIES.value: unit_amenities,
-            TableHeaders.BUILDING_AMENITIES.value: building_amenities,
-            TableHeaders.PETS.value: pets_value,
-            TableHeaders.LAT.value: row[TableHeaders.LAT.value],
-            TableHeaders.LON.value: row[TableHeaders.LON.value]
-        })
-    return cleaned_list
-
-# Function to flatten data
-def flatten_data(data):
-    flattened_data = []
-    for entry in data:
-        flattened_entry = entry.copy()
-        for key, value in entry.items():
-            if isinstance(value, dict):
-                for sub_key, sub_value in value.items():
-                    flattened_entry[sub_key] = sub_value
-        flattened_entry.pop(TableHeaders.UNIT_AMENITIES.value, None)
-        flattened_entry.pop(TableHeaders.BUILDING_AMENITIES.value, None)
-        flattened_data.append(flattened_entry)
-    return flattened_data
+    return df
