28625cc2631f82e1027b3d5351d9eb4bf7ce7340
alyssadsouza <alyssa.dsouza63@gmail.com>
2023-12-30 23:40:31 -0500

    drop tables

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 
-from backend import db_models
-from backend.database import engine
+from backend.database import engine, Base, SessionLocal
 from backend.routers import predict
 from backend.routers import analysis
 
-db_models.Base.metadata.create_all(bind=engine)
+# Create a session
+session = SessionLocal()
+
+# Clear all data from tables associated with the Base
+Base.metadata.drop_all(engine)
+
+# Recreate tables after dropping them (if needed)
+Base.metadata.create_all(engine)
+
+# Commit changes and close the session
+session.commit()
+session.close()
 
 app = FastAPI()
 
